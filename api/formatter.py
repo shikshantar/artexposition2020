867896c694d0a0b489bf29efecac8fe3a1c6ab19
arnav rustagi <51513216+argonsodiumvanadium@users.noreply.github.com>
2021-01-17 20:28:56 +0530

    Add files via upload

--- api/formatter.py
+++ api/formatter.py
@@ -1,50 +1,63 @@
 import os
+import json
 from PIL import Image
 
 def main ():
-    # move all the images from directories to this directory 
-    os.system("mv ./formatted/*_*/* .")
-    
+    # move all the images from directories to this directory
+
     mypath = "."
-    files = [f for f in os.listdir(mypath) if os.isfile(os.join(mypath, f) and f.endswith("jpg")]
+    files = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f)) and " " in f]
+
+
+    directories = []
 
     for f in files:
-        constructDirectoriesAndJson(os.join(mypath, f), name.split("_"), file_name)
-    
-    createInode(my_path)
+        try:
+            dirname = constructDirectoriesAndJson(os.path.join(mypath, f), f.split("_"), f)
+            if dirname != None:
+                directories.append(dirname)
+        except  :
+            print(f)
+
+    print(directories)
+
+    for directory in directories:
+        print("l")
+        createInode(directory)
 
 
 # path is the path to the given picture
 # name_vars is basically all the data in the name
 def constructDirectoriesAndJson (img_path, name_vars, file_name):
-    THUMBNAIL_SIZE = (757,757)    
+    THUMBNAIL_SIZE = (757,757)
+    directory = None
 
 
     dirname = "./pictures/"+name_vars[0] + "_" + name_vars[1]
 
     if not os.path.isdir(dirname):
         os.mkdir(dirname)
+        print(dirname)
+        directory = dirname
 
     project_name = name_vars[2]
 
-    project_path = os.join(dirname,project_name)
+    project_path = os.path.join(dirname,project_name)
     os.mkdir(project_path)
 
     img = Image.open(img_path)
     img.thumbnail(THUMBNAIL_SIZE)
-    img.save(os.join(project_path,"thumbnail.jpg"))
+    img.save(os.path.join(project_path,"thumbnail.jpg"))
 
-    os.rename(img_path,os.join(project_path,file_name))
+    os.rename(img_path,os.path.join(project_path,file_name))
+
+    return directory
 
 def createInode(path):
-    inode = {
-        "directories" : []
-        "files" : []
-    }
-    for f in os.listdir():
-        if os.isdir(f):
-            inode["directories"].append(f)
-        else:
-            inode["files"].append(f)
+    data = json.dumps(os.listdir(path))
+    inode = open(os.path.join(path,"inode.json"), "w+")
+    inode.write(data)
+    inode.close()
 
-    json.dump(inode,open("inode.json", "w+"))
+
+main()
